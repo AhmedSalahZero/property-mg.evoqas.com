e326ae08f7ad1403272c410a64638deae446abcd
Ahmed Salah <asalahdev5@gmail.com>
2026-07-21 16:15:00 +0300

    upload propert mg

--- database/migrations/2026_07_14_000001_create_corporate_expenses_table.php
+++ database/migrations/2026_07_14_000001_create_corporate_expenses_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
 {
     public function up(): void
     {
+		Schema::disableForeignKeyConstraints();
 		Schema::dropIfExists('corporate_expenses');
         Schema::create('corporate_expenses', function (Blueprint $table) {
             $table->id();
